aea209fbb913d85f8bdd928b08336423083f67dc
Hồ Việt Anh <138989039+AndyAnh174@users.noreply.github.com>
2025-01-29 10:02:31 +0700

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiUrl: import.meta.env.VITE_API_URL || 'https://dining-scientific-shanghai-demonstrates.trycloudflare.com',
+  apiUrl: import.meta.env.VITE_API_URL || 'https://adjusted-some-suspected-yacht.trycloudflare.com',
   defaultAvatar: import.meta.env.VITE_DEFAULT_AVATAR || '/static/images/members/default-avatar.png',
 }; 
